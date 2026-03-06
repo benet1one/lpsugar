cc8d5dd95cc4e174a6ab741410bd8b27279e8a67
benetsugarboy <benet1one@gmail.com>
2026-03-06 17:33:44 +0100

    Work on lp_problem

--- R/lp.R
+++ R/lp.R
@@ -4,14 +4,34 @@ lp_problem <- function() {
     list(
         variables = list(),
         objective = numeric(),
-        constraints = new_constraints(),
+        constraints = list(),
 
+        # Must equal length of objective coefficients.
         .nvar = 0L,
+        # Names of variables with their respective indices, e.g. "x[A, 2]".
         .varnames = character()
 
     ) |> structure(class = "lp_problem")
 }
 
-new_constraints <- function() {
-    list()
+#' @export
+print.lp_problem <- function(x, ...) {
+    cat("<SugarLP Linear Problem>\n\n")
+
+    if (length(x$variables) > 0L) {
+        cat("-- $variables --\n")
+        print(x$variables)
+    }
+
+    if (any(x$objective != 0L)) {
+        cat("-- $objective --\n")
+        print(x$objective)
+    }
+
+    if (length(x$constraints) > 0L) {
+        cat("-- $constraints --")
+        print(x$constraints)
+    }
+
+    cat("\n")
 }
