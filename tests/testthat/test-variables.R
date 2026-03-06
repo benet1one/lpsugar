fe521d1d246275ad6a308f3dc1cbea875ef80f8d
benetsugarboy <benet1one@gmail.com>
2026-03-06 17:34:02 +0100

    Update test-variables.R

--- tests/testthat/test-variables.R
+++ tests/testthat/test-variables.R
@@ -8,12 +8,36 @@ parse_variable_definition(y[a, 1:5])
 parse_variable_definition(t[a, b = 1:5])
 
 
-testthat::expect_error(
-    parse_variable_definition(x[, 1:3])
-)
-testthat::expect_error(
-    parse_variable_definition(x[b])
-)
-testthat::expect_error(
-    parse_variable_definition(mean(1))
-)
+test_that("variable definitions", {
+    testthat::expect_error(
+        parse_variable_definition(x[, 1:3])
+    )
+    testthat::expect_error(
+        parse_variable_definition(x[b])
+    )
+    testthat::expect_error(
+        parse_variable_definition(mean(1))
+    )
+})
+
+
+p <- lp_problem() |>
+    lp_variable(x, integer = TRUE) |>
+    lp_variable(y[a], binary = TRUE) |>
+    lp_variable(z[1:2, a], lower = 0)
+
+print(p)
+
+p$variables$y[1:2]
+p$variables$y[-3]
+p$variables$y[c(TRUE, TRUE, FALSE)]
+
+p$variables$y["b"]
+p$variables$y[c("a", "b")]
+
+
+testthat::test_that("variable indexing", {
+    testthat::expect_error(p$variables$y[4])
+    testthat::expect_error(p$variables$y[0])
+    testthat::expect_error(p$variables$y["d"])
+})
