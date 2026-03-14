8f113205a2d6bcbd89e0e0a073ecdf7e1d1347fe
benetsugarboy <benet1one@gmail.com>
2026-03-14 08:42:50 +0100

    for_split will not be supported in alias definitions
    The implementation is too difficult and likely to spawn unexpected bugs.

--- R/alias.R
+++ R/alias.R
@@ -13,6 +13,10 @@ lp_alias <- function(.problem, ...) {
     dots <- rlang::enquos(...)
     nams <- rlang::names2(dots)
 
+    if (any(nams == "")) {
+        abort("Aliases must be named.")
+    }
+
     for (d in seq_along(dots)) {
         data <- data_mask(.problem)
         .problem <- lp_alias_internal(.problem, dots[[d]], nams[d], data)
@@ -22,45 +26,20 @@ lp_alias <- function(.problem, ...) {
 }
 
 lp_alias_internal <- function(.problem, quosure, name, data) {
-    fs <- for_split(quosure, evaluate = FALSE)
-    is_fs <- is_for_split(fs)
-    is_named <- name != ""
-
-    if (is_named + is_fs != 1L) {
-        abort("Either name the alias or use `for` notation. See ?lp_alias for details.",
-              call = parent.frame())
+    if (name %in% names(.problem$variables)) {
+        abort("Cannot override variable `{new_alias$name}`.", call = parent.frame())
+    } else if (name %in% names(.problem$aliases)) {
+        inform("Overriding alias `{new_alias$name}`.", call = parent.frame())
     }
 
-    new_alias <- if (is_named) {
-        lp_alias_named(quosure, name, data)
-    } else {
-        lp_alias_fs(fs, data)
-    }
-
-    if (new_alias$name %in% names(.problem$variables)) {
-        abort("Cannot override variable `{new_alias$name}`", call = parent.frame())
-    } else if (new_alias$name %in% names(.problem$aliases)) {
-        inform("Overriding alias `{new_alias$name}`", call = parent.frame())
-    }
-
-    # $value is an lp_variable(), so its name is overwritten
-    new_alias$value$name <- new_alias$name
-    .problem$aliases[[new_alias$name]] <- new_alias$value
-    return(.problem)
-}
-
-lp_alias_named <- function(quosure, name, data) {
     value <- rlang::eval_tidy(quosure, data = data)
 
     if (!is_lp_variable(value)) {
-        abort("Did not evaluate to a variable", call = quosure)
+        abort("Did not evaluate to a variable.", call = quosure)
     }
 
-    list(name = name, value = value)
-}
-
-lp_alias_fs <- function(fs, data) {
-    abort("Not yet supported", call = parent.frame())
+    .problem$aliases[[name]] <- value
+    return(.problem)
 }
 
 # Alias --------------------
