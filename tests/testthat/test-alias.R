490b6eb0742a5c02b8badb2fa64dd0305383ea6b
benetsugarboy <benet1one@gmail.com>
2026-03-22 15:44:37 +0100

    Improved alias tests for now

--- tests/testthat/test-alias.R
+++ tests/testthat/test-alias.R
@@ -1,19 +1,16 @@
 
 
-p <- lp_problem() |>
-    lp_variable(x[1:2, 1:2]) |>
-    lp_alias(
-        sum_x = sum(x),
-        # for (i in 1:2) rowsum_x[i] = sum(x[i, ]),
+test_that("alias", {
+    p <- lp_problem() |>
+        lp_var(x[1:2, 1:3])
 
-        # Alternatives?
-        # rowsum_x[i = 1:2] = sum(x[i, ]),
-        # rowsum_x = for (i in 1:2) sum(x[i, ])
+    p |> lp_alias(
+        sum_x = sum(x),
+        # colsums_x[j = 1:3] <- sum(x[, j])
     )
 
-p$aliases
-
-test_that("alias_named", {
-    expect_error(lp_alias(p, sum(x)))
-    expect_error(lp_alias(p, rowsum_x = for (i in 1:2) rowsum_x[i] = sum(x[i, ])))
+    expect_error(
+        lp_alias(p, sum(x)),
+        "must be named"
+    )
 })
