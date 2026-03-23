7e831e9ab17746ceafb3a9f26fb54c51ea4a7fce
benetsugarboy <benet1one@gmail.com>
2026-03-23 12:29:30 +0100

    Worked on objective tests

--- tests/testthat/_snaps/objective.md
+++ tests/testthat/_snaps/objective.md
@@ -1,4 +1,4 @@
-# objective snaps
+# objective
 
     Code
       lp_maximize(p, -x)$objective
@@ -18,5 +18,22 @@
       $coef
            x   y[a]   y[b]   y[c] z[1,a] z[2,a] z[1,b] z[2,b] z[1,c] z[2,c] 
            0      1      1      1      0      0      0      0      0      0 
+      
+
+---
+
+    Code
+      lp_minimize(p, {
+        i <- 1:2
+        j <- 1:3
+        z[i, j] * outer(i, j, `^`)
+      })$objective
+    Message
+      Summing variables in objective. Write `sum(...)` to suppress this message.
+    Output
+      minimize {     i <- 1:2     j <- 1:3     z[i, j] * outer(i, j, `^`) } 
+      $coef
+           x   y[a]   y[b]   y[c] z[1,a] z[2,a] z[1,b] z[2,b] z[1,c] z[2,c] 
+           0      0      0      0      1      2      1      4      1      8 
       
 
--- tests/testthat/test-objective.R
+++ tests/testthat/test-objective.R
@@ -1,17 +1,14 @@
+test_that("objective", {
+    a <- letters[1:3]
 
-a <- letters[1:3]
+    p <- lp_problem() |>
+        lp_variable(x, integer = TRUE) |>
+        lp_variable(y[a], binary = TRUE) |>
+        lp_variable(z[1:2, a], lower = 0)
 
-p <- lp_problem() |>
-    lp_variable(x, integer = TRUE) |>
-    lp_variable(y[a], binary = TRUE) |>
-    lp_variable(z[1:2, a], lower = 0)
-
-test_that("objective snaps", {
     expect_snapshot(lp_maximize(p, -x) $ objective)
     expect_snapshot(lp_minimize(p, sum(y)) $ objective)
-})
 
-test_that("sum message", {
     expect_message(lp_minimize(p, y), "sum\\(y\\)")
     expect_message(
         p |> lp_minimize({
@@ -19,5 +16,13 @@ test_that("sum message", {
             k*y
         }),
         "sum"
+    )
+
+    expect_snapshot(
+        p |> lp_minimize({
+            i <- 1:2
+            j <- 1:3
+            z[i, j] * outer(i, j, `^`)
+        }) |> _$objective
     )
 })
