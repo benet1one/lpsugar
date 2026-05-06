d02f94b31c9a70e198d8a357f8517c8836db161a
benetsugarboy <benet1one@gmail.com>
2026-05-06 10:52:26 +0200

    Best syntax for ifelse_l

--- R/custom_fun.R
+++ R/custom_fun.R
@@ -279,33 +279,36 @@ ifelse_v <- function(test, yes, no) {
     no + test * (yes - no)
 }
 ifelse_l <- function(test, yes, no) {
+    # Default R code from `ifelse`
     if (is.atomic(test)) {
-        if (typeof(test) != "logical")
+        if (typeof(test) != "logical") {
             storage.mode(test) <- "logical"
+        }
+
         if (length(test) == 1 && is.null(attributes(test))) {
-            if (is.na(test))
+            if (is.na(test)) {
                 return(NA)
-            else if (test) {
-                if (length(yes) == 1) {
-                    yat <- attributes(yes)
-                    if (is.null(yat) || (is.function(yes) && identical(names(yat),
-                                                                       "srcref")))
-                        return(yes)
+            } else if (test && length(yes) == 1) {
+                yat <- attributes(yes)
+                if (is.null(yat) || (is.function(yes) && identical(names(yat), "srcref"))){
+                    return(yes)
+                }
+            } else if (length(no) == 1) {
+                nat <- attributes(no)
+                if (is.null(nat) || (is.function(no) && identical(names(nat), "srcref"))) {
+                    return(no)
                 }
             }
-            else if (length(no) == 1) {
-                nat <- attributes(no)
-                if (is.null(nat) || (is.function(no) && identical(names(nat),
-                                                                  "srcref")))
-                    return(no)
-            }
+        }
+    } else {
+        test <- if (isS4(test)) {
+            methods::as(test, "logical")
+        } else {
+            as.logical(test)
         }
     }
-    else test <- if (isS4(test))
-        methods::as(test, "logical")
-    else as.logical(test)
 
-
+    # lpsugar code
     len <- length(test)
 
     # One of them is a variable
