02b3ec34125a33a6f8a1fd55a472b499d69716fd
benetsugarboy <benet1one@gmail.com>
2026-08-01 14:25:03 +0200

    Fixed example for objective function

--- inst/examples/example_objective.R
+++ inst/examples/example_objective.R
@@ -15,7 +15,7 @@ library(ROI.plugin.highs)
 (s <- lp_solve(p))
 
 s$aliases$total_profit
-sum(p$objective$L * s$variables_vec)
+sum(c(p$objective$L) * s$variables_vec)
 
+s$aliases$total_profit - fix_cost
 s$objective
-sum(p$objective$L * s$variables_vec) + p$objective$A
