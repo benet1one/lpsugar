e5ad8754a18cb50dcdcf858fab96719ba244654c
benetsugarboy <benet1one@gmail.com>
2026-08-03 10:59:12 +0200

    Nonlinear objective now in examples

--- inst/examples/example_objective.R
+++ inst/examples/example_objective.R
@@ -1,3 +1,5 @@
+# Linear Objective using an Alias ---------------
+
 profit   <- c(Phone = 60, Tablet = 20, eBook = 10)
 max_made <- c(Phone = 500, Tablet = 300, eBook = 950)
 product  <- names(profit)
@@ -12,10 +14,33 @@ p <- lp_problem() |>
 p$objective
 
 library(ROI.plugin.highs)
-(s <- lp_solve(p))
+s <- lp_solve(p)
+print(s)
 
 s$aliases$total_profit
 sum(c(p$objective$L) * s$variables_vec)
 
 s$aliases$total_profit - fix_cost
 s$objective
+
+
+# Nonlinear objective ---------------------------
+
+nl <- lp_problem() |> 
+    lp_variable(x, lower = 0) |> 
+    lp_variable(y, lower = 0) |> 
+    lp_maximize(nonlinear(sqrt(x) * log(y))) |> 
+    lp_constraint(x + y <= 10)
+
+# There are some different solvers within `nloptr`
+library(ROI.plugin.nloptr)
+lpsugar_applicable_solvers(nl)
+
+lp_solve(
+    nl, 
+    solver = "nloptr.cobyla", 
+    start = list(x = 1, y = 1)
+)
+
+# See more examples in the Nonlinear vignette
+# vignette("nonlinear", package = "lpsugar")
